0072746767646f5e6c9cb0746eba21f9fc358d0c
Povar313 <oleg.povarov96@gmail.com>
2024-10-03 15:19:47 +0200

    task_update_1

--- task_1.py
+++ task_1.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 
 def get_days_from_today(date):
+    try:
+        target_date = datetime.strptime(date, '%Y-%m-%d').date()
+        
+        current_date = datetime.today().date()
+        
+        delta = (target_date - current_date).days
+        
+        return delta
 
-    target_date = datetime.strptime(date, '%Y-%m-%d')
-
-    current_date = datetime.today()
-
-    delta = (target_date - current_date).days
-
-    return delta
-
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте 'РРРР-ММ-ДД'."
 
 days_difference = get_days_from_today("2021-10-09")
-
-print("Кількість днів між сьогоднішньою датою і заданою:", days_difference)
+print(f"Кількість днів між сьогоднішньою датою і заданою: {days_difference}")
